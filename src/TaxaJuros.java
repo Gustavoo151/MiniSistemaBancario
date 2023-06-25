e68394f2db031f8e622dacae71b1b12bce5ab54f
Gustavo de oliveira <71619573+Gustavoo151@users.noreply.github.com>
2023-06-25 00:40:11 -0300

    Classe TaxaJuros

---
+++ src/TaxaJuros.java
@@ -0,0 +1,11 @@
+public class TaxaJuros {
+    private static double taxajuros;
+
+    public static double  getTaxa(){
+        return taxajuros;
+    }
+
+    public void setTaxa(double juros){
+        taxajuros = juros;
+    }
+}
--- src/interfaces/TaxaJuros.java
+++
@@ -1,5 +0,0 @@
-package interfaces;
-
-static interface TaxaJuros {
-
-}
