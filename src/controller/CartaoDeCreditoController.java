35ac3ec762cb63981b32c27d2ab7a31f93534e56
Gustavo de oliveira <71619573+Gustavoo151@users.noreply.github.com>
2023-06-27 23:24:35 -0300

    User Controller Finalizado

---
+++ src/controller/CartaoDeCreditoController.java
@@ -0,0 +1,9 @@
+package controller;
+
+public class CartaoDeCreditoController {
+
+
+    public void operacoes(){
+
+    }
+}
--- src/controller/UserController.java
+++ src/controller/UserController.java
@@ -29,8 +29,8 @@ public class UserController {
             }
             case 4 -> {
                 this.setDadosCliente();
-                Conta cartaoDeCredito = new CartaoDeCretido(this.nome, this.cpf, this.saldoValor);
-                System.out.println("Conta Cartão Crédito Criada.");
+                CartaoDeCreditoController cartaoDeCredito = new CartaoDeCreditoController(this.nome, this.cpf, this.saldoValor);
+                cartaoDeCredito.operacoes();
             }
 
             default -> System.out.println("Opção inválida!");
@@ -47,7 +47,7 @@ public class UserController {
         System.out.println("Digite o Saldo Inicial:");
         this.saldoValor = input.nextDouble();
     }
-    
+
     private void menu(){
         Scanner input = new Scanner(System.in);
         System.out.println("Digite qual tipo de você deseja criar: \n1- Conta Corrente.\n2- Conta Poupança.\n3- Investimento Cli.\n4- Cartão de Crédito.");
