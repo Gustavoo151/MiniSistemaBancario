771c9e3b77bf52b4be76cd2cf9bd93266d6edfca
Gustavo de oliveira <71619573+Gustavoo151@users.noreply.github.com>
2023-07-20 18:16:09 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,8 @@ import controller.UserController;
 public class Main {
     public static void main(String[] args) {
 
-        UserController userController = new UserController();
-        userController.CriacaoConta();
+        UserController contaUsuario = new UserController();
+        contaUsuario.CriacaoConta();
+
     }
 }
